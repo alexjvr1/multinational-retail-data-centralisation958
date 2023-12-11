a519129ab6264b95adfaf3e6dd5ea673a2b0dba2
alexjvr1 <alexjvr@gmail.com>
2023-12-11 02:28:07 +0000

    create a function retrieve_pdf_data that uses tabuly-py to retrieve a table from a pdf

--- data_extraction.py
+++ data_extraction.py
@@ -1,12 +1,11 @@
 #Python script to extract data from different data sources: 
 #CSV, API, and S3 bucket
-
-#import required modules
-from database_utils.py import init_db_engine
-from sqlalchemy import inspect
-import pandas as pd
 class DataExtractor:
-        def __init__(self)
+#import required modules
+	from database_utils import DatabaseConnector
+	DatabaseConnector = DatabaseConnector()
+	from sqlalchemy import inspect
+	import pandas as pd
 #function to list all the tables in the database
 	def list_db_tables():
 		engine = init_db_creds()
@@ -18,8 +17,16 @@ class DataExtractor:
 		return table_names
 #Function to extract a db table to a pandas df
 	def read_rds_table(DatabaseConnector, table_name):
+		engine = init_db_creds()
 		table_name = table_name
 		table_df = pd.read_sql_table(table_name, engine)
 		table_df.head()
 		return table_df
-
+#Function to extract pdf data to a pandas data frame
+#The function takes as input a link to a pdf
+	def retrieve_pdf_data(pdf_path):
+		import tabula
+		pdf_path = pdf_path
+		dfs = pd.concat(tabula.read_pdf(pdf_path, lattice=True,pag
+es='all', multiple_tables=True))
+		return dfs
