289b58b4cf300c5b0545125efe11e0cc6e8688d1
alexjvr1 <alexjvr@gmail.com>
2023-12-12 15:31:53 +0000

    initiate instances of DataExtractor and DatabaseConnector. Made corrections to Class initialisation

--- data_extraction.py
+++ data_extraction.py
@@ -1,14 +1,21 @@
 #Python script to extract data from different data sources: 
 #CSV, API, and S3 bucket
+
+#import all dependencies
+from sqlalchemy import inspect
+import pandas as pd
+import tabula
+
+#All functions related to data extraction
 class DataExtractor:
-#import required modules
-	from database_utils import DatabaseConnector
-	DatabaseConnector = DatabaseConnector()
-	from sqlalchemy import inspect
-	import pandas as pd
+	def __init__(self):
+		self = self
+#create an instance of the DatabaseConnector class (from database_utils)
+de = DataExtractor()
+dc = DatabaseConnector()
 #function to list all the tables in the database
 	def list_db_tables():
-		engine = init_db_creds()
+		engine = dc.init_db_creds()
 		#create an inspector
 		inspector = inspect(engine)
 		table_names = []
@@ -17,7 +24,8 @@ class DataExtractor:
 		return table_names
 #Function to extract a db table to a pandas df
 	def read_rds_table(DatabaseConnector, table_name):
-		engine = init_db_creds()
+		DatabaseConnector = dc
+		engine = dc.init_db_creds()
 		table_name = table_name
 		table_df = pd.read_sql_table(table_name, engine)
 		table_df.head()
@@ -25,8 +33,6 @@ class DataExtractor:
 #Function to extract pdf data to a pandas data frame
 #The function takes as input a link to a pdf
 	def retrieve_pdf_data(pdf_path):
-		import tabula
 		pdf_path = pdf_path
-		dfs = pd.concat(tabula.read_pdf(pdf_path, lattice=True,pag
-es='all', multiple_tables=True))
+		dfs = pd.concat(tabula.read_pdf(pdf_path, lattice=True,pages='all', multiple_tables=True))
 		return dfs
