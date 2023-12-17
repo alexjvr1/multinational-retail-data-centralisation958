2d541f54cd87d7c8e72330339cf482ff1f2479eb
alexjvr1 <alexjvr@gmail.com>
2023-12-17 13:35:31 +0000

    create function retrieve_stores_data to loop through a list of urls and return a pandas df of information from an api

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -6,6 +6,7 @@ from sqlalchemy import inspect
 import pandas as pd
 import tabula
 from database_utils import DatabaseConnector
+import requests
 
 #All functions related to data extraction
 class DataExtractor:
@@ -30,7 +31,25 @@ class DataExtractor:
 
 #Function to extract pdf data to a pandas data frame
 #The function takes as input a link to a pdf
-	def retrieve_pdf_data(pdf_path):
-		pdf_path = pdf_path
+	def retrieve_pdf_data(self, pdf_path):
 		dfs = pd.concat(tabula.read_pdf(pdf_path, lattice=True,pages='all', multiple_tables=True))
 		return dfs
+	
+#Function to return the number of stores that there are data for at an API end point
+	def list_number_of_stores(self, number_of_stores_endpoint, api_header_dict):
+		url = number_of_stores_endpoint
+		header_dict = api_header_dict
+		number_of_stores=requests.get(url, headers=header_dict)
+		return number_of_stores
+
+#Function to return the store data for the stores found in list_number_of_stores
+	def retrieve_stores_data(self, retrieve_stores_endpoint, api_header_dict, number_of_stores):
+		url=retrieve_stores_endpoint  #url without the store number
+		header_dict=api_header_dict  #authentication key for access to the end point
+		store_data=[]   #create an empty list where all the store date will be stored
+		#loop through the urls to retrieve store data for each store and appends as a json object to the list
+		for i in range(1,number_of_stores):  
+			store_data.append(requests.get(url=(url+str(i)), headers=header_dict).json())
+		#convert to pandas df
+		store_data_df = pd.DataFrame.from_records(store_data)
+		return store_data_df
