e73cff9ed2971153cb6885bceb764e5023a838cf
alexjvr1 <alexjvr@gmail.com>
2024-01-17 14:55:38 +0000

    add foreign keys to orders_table by linking to all 5 dim_ tables. Two of these work, 2 need the primary keys set, and entries in one doesn't match up with the dim_ table

--- src/sales_data_queries.sql
+++ src/sales_data_queries.sql
@@ -239,3 +239,31 @@ ADD PRIMARY KEY (store_code)
 
 ALTER TABLE dim_products
 ADD PRIMARY KEY (product_code)
+
+/* Task 9: Add foreign key to orders_table */
+
+ALTER TABLE orders_table   --NEED TO ADD PRIMARY KEY TO CARD_DETAILS first
+	ADD CONSTRAINT fk_card_number 
+		FOREIGN KEY (card_number) 
+			REFERENCES dim_card_details (card_number); 
+
+ALTER TABLE orders_table  --SUCCESS
+	ADD CONSTRAINT fk_date_uuid 
+		FOREIGN KEY (date_uuid) 
+			REFERENCES dim_date_times (date_uuid);
+			
+ALTER TABLE orders_table  --SUCCESS
+	ADD CONSTRAINT fk_product_code 
+		FOREIGN KEY (product_code) 
+			REFERENCES dim_products (product_code); 
+
+ALTER TABLE orders_table  --Need to add primary key to store_details
+	ADD CONSTRAINT fk_store_code 
+		FOREIGN KEY (store_code) 
+			REFERENCES dim_store_details (store_code); 
+
+ALTER TABLE orders_table  --PROBLEM with some entries not matching
+	ADD CONSTRAINT fk_user_uuid 
+		FOREIGN KEY (user_uuid) 
+			REFERENCES dim_users_table (user_uuid); 
+
