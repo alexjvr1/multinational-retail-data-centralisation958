da18f7167f0f95820e2a206524ce78ed650f848e
alexjvr1 <alexjvr@gmail.com>
2024-01-19 15:52:14 +0000

    cast to numberic and round to 2 decimal places for the total_sales column in the third SQL query

--- src/sales_data_queries.sql
+++ src/sales_data_queries.sql
@@ -294,12 +294,12 @@ ORDER BY count DESC;   --order by descending
 
 
 -- Which months produced the largest amount of sales (i.e. cost)?
-
-SELECT  dim_date_times.month, SUM(orders_table.product_quantity*dim_products.product_price) AS total_sales  --SUM to get the aggregate when grouping by a different column
+    -- Cast to numeric to be able to ROUND to two decimal places
+SELECT  dim_date_times.month, ROUND(CAST(SUM(orders_table.product_quantity*dim_products.product_price) AS NUMERIC),2) AS total_sales --SUM to get the aggregate when grouping by a different column
 	FROM orders_table
 		LEFT JOIN dim_products
 		ON dim_products.product_code = orders_table.product_code
 		LEFT JOIN dim_date_times
 		ON dim_date_times.date_uuid = orders_table.date_uuid
-	GROUP BY dim_date_times.month   
+	GROUP BY dim_date_times.month
 	ORDER BY total_sales DESC; 
